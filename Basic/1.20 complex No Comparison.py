b5104a0e524be28c05e0ea2504e36e358aa5f272
Harsh Meena <122894033+harshmeena0645@users.noreply.github.com>
2025-05-01 19:32:19 +0530

    1.20 complex No Comparison.py

--- Basic/1.20 complex No Comparison.py
+++ Basic/1.20 complex No Comparison.py
@@ -4,8 +4,8 @@ real_part = num.real
 imaginary_part = num.imag
 
 if real_part > imaginary_part:
-    print(f"The real part ({real_part}) is greater than the imaginary part ({imaginary_part}).")
+    print(real_part ,' is greater than ',imaginary_part)
 elif imaginary_part > real_part:
-    print(f"The imaginary part ({imaginary_part}) is greater than the real part ({real_part}).")
+    print(imaginary_part,' is greater than ',real_part)
 else:
-    print(f"The real part ({real_part}) and the imaginary part ({imaginary_part}) are equal.")
+    print(real_part , imaginary_part,'are equal')
