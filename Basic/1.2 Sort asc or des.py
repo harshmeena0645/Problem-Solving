ea7303ed8f98b6cae39d913099d8f1b777a1b2b5
Harsh Meena <122894033+harshmeena0645@users.noreply.github.com>
2025-06-02 08:09:45 +0530

    1.2 Sort asc or des.py

--- Basic/1.2 Sort asc or des.py
+++ Basic/1.2 Sort asc or des.py
@@ -7,3 +7,17 @@ desc_sorted = sorted(arr, reverse=True)
 print("Original array:", arr)
 print("Ascending order:", asc_sorted)
 print("Descending order:", desc_sorted)
+
+
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
+print(thislist)
+
+output ['Kiwi', 'Orange', 'banana', 'cherry']
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+
+output ['banana', 'cherry', 'Kiwi', 'Orange']
