81f07d8d97267165bfd40fa6732cfeadee810968
Harsh Meena <122894033+harshmeena0645@users.noreply.github.com>
2025-07-02 09:37:33 +0530

    Update 1.2 Sort asc or des.py

--- Basic/1.2 Sort asc or des.py
+++ Basic/1.2 Sort asc or des.py
@@ -1,7 +1,6 @@
 arr = [50, 20, 40, 10, 30]
 
 asc_sorted = sorted(arr)
-
 desc_sorted = sorted(arr, reverse=True)
 
 print("Original array:", arr)
@@ -25,7 +24,7 @@ output ['banana', 'cherry', 'Kiwi', 'Orange']
 
 
 def myfunc(n):
-return abs(n - 50)
+    return abs(n - 50)
 thislist = [100, 50, 65, 82, 23]
 thislist.sort(key = myfunc)
 print(thislist)
