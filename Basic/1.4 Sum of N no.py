78cba1a284a3e0a98adc3b807180efc53f622356
Harsh Meena <122894033+harshmeena0645@users.noreply.github.com>
2025-08-03 00:23:12 +0530

    Update 1.4 Sum of N no.py

--- Basic/1.4 Sum of N no.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Enter a number: "))
-
-if n >= 0:
-    sum_of_numbers = n * (n + 1) // 2
-    print(f"The sum of the first {n} natural numbers is: {sum_of_numbers}")
-else:
-    print("Please enter a non-negative integer.")
